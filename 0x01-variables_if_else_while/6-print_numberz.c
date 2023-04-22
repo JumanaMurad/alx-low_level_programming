b159a99fcf12e809371f9775719aae304a2f057e
JumanaMurad <jumanamurad1@gmail.com>
2023-04-21 17:41:42 -0700

    if

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.d>
+/**
+  * main - entry point
+  *
+  * Return: always 0
+  */
+int main(void)
+{
+	int n;
+	
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
