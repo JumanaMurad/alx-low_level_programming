996d6817fd1c08e106454c3de988abff51debdd4
JumanaMurad <jumanamurad1@gmail.com>
2023-04-21 17:35:42 -0700

    if

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+  * main - entry point
+  *
+  * Return: always 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 11; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
