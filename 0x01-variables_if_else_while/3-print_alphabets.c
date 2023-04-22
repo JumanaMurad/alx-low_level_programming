19b2f9ddc2d50c631f0ad535ca84db2fbea3922e
JumanaMurad <jumanamurad1@gmail.com>
2023-04-21 17:24:33 -0700

    if

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+  * main - entry point
+  *
+  * Return: always 0
+  */
+int main(void)
+{
+	int x = 97;
+	int y = 65;
+
+	while (x < 123)
+	{
+		putchar(x);
+	}
+	while (m < 91)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
